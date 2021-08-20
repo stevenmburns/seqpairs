2f34005142ea8e3587072ba3f4221dddf67b5f69
Steven Burns <smburns47@gmail.com>
2021-08-20 13:08:02 -0700

    Added iterator

--- varol_rotem.py
+++ varol_rotem.py
@@ -1,6 +1,8 @@
+import pytest
 import itertools
+import functools
 
-def enum( n, relation):
+def gen_enum( n, relation):
     a = list(range(n+1))
     ap = list(range(n+1))
 
@@ -29,7 +31,50 @@ def enum( n, relation):
             yield a, ap
             k = n
 
-def test_A():
+class EnumIter:
+    def __init__(self, n, relation):
+        self.n = n
+        self.relation = relation
+
+    def __iter__(self):
+        self.a = list(range(self.n+1))
+        self.ap = list(range(self.n+1))
+
+        self.first = True
+
+        return self
+
+    def __next__(self):
+        if self.first:
+            self.first = False
+            return self.a, self.ap
+
+        k = self.n
+        while True:
+            j = self.ap[k]
+            l = self.a[j-1]
+            if (l,k) in self.relation:
+                while j < k:
+                    l = self.a[j+1]
+                    self.a[j] = l
+                    self.ap[l] = j
+                    j += 1
+                self.ap[k] = k
+                self.a[k] = self.ap[k]
+                k -= 1
+                if k == 0:
+                    raise StopIteration
+            else:
+                # V4
+                self.a[j-1] = k
+                self.a[j] = l
+                self.ap[k] = j-1
+                self.ap[l] = j
+                return self.a, self.ap
+
+
+
+def mk_relation(n):
     relation = set()
     def product( s0, s1):
         for a,b in itertools.product( s0, s1):
@@ -40,14 +85,49 @@ def test_A():
             for j in range(i+1,len(s)):
                 relation.add( (s[i], s[j]))
 
-    n = 4
-
     product( [0], list(range(1,n+1)))
 
     #chain( list(range(1,n+1)))
     product([1], [2])
     product([2], [3])
     product([3], [4])
+    return relation
 
-    for a, ap in enum( n, relation):
+examples = [(5,mk_relation(5)), (4,mk_relation(4))]
+
+@pytest.mark.parametrize("n,relation", examples)
+def test_A(n,relation):
+    print(n,relation)
+    for a, ap in gen_enum( n, relation):
         print(a,ap)
+
+@pytest.mark.parametrize("n,relation", examples)
+def test_B(n,relation):
+    print(n,relation)
+    for a, ap in iter(EnumIter( n, relation)):
+        print(a,ap)
+
+def runit(n=10):
+    relation = set()
+    def product( s0, s1):
+        for a,b in itertools.product( s0, s1):
+            relation.add( (a,b))
+
+    def chain( s):
+        for i in range(len(s)):
+            for j in range(i+1,len(s)):
+                relation.add( (s[i], s[j]))
+
+    product( [0], list(range(1,n+1)))
+
+    #chain( list(range(1,n+1)))
+    #product([1], [2])
+    #product([2], [3])
+    #product([3], [4])
+
+    return sum(1 for (a,ap) in iter(EnumIter( n, relation)))
+
+@pytest.mark.skip
+def test_AB(benchmark):
+    count = benchmark(runit)
+    assert functools.reduce(lambda a,b: a*b, range(1,11)) == count
